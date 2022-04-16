e91e67504f6627bf5c9170e5bf0b08e45fbfbfb6
carghai74 <carghai74@gmail.com>
2022-04-15 23:08:23 -0400

    Initial Commit

---
+++ MessAround/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MessAround
+//
+//  Created by Ekam Ghai on 4/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MessAround/MessAroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessAroundApp.swift
+//  MessAround
+//
+//  Created by Ekam Ghai on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct MessAroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
